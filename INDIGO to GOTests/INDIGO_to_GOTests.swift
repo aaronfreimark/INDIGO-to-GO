0c0f3d796c01e138e2464e3018c0a55579504712
Aaron Freimark <aaron@tekserve.com>
2020-09-27 13:56:48 -0400

    rename final?

--- INDIGO to GOTests/INDIGO_to_GOTests.swift
+++ INDIGO to GOTests/INDIGO_to_GOTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import INDIGO_Status
+@testable import INDIGO_To_Go
 
-class INDIGO_StatusTests: XCTestCase {
+class INDIGO_to_GOTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- INDIGO to GOUITests/INDIGO_to_GOUITests.swift
+++ INDIGO to GOUITests/INDIGO_to_GOUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class INDIGO_StatusUITests: XCTestCase {
+class INDIGO_to_GOUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
